dcd8843e22dd7ce4051fd38e5d1d81b97c441734
kantosev <103773083+kantosev@users.noreply.github.com>
2022-04-15 22:10:04 +0300

    Initial Commit

---
+++ ManagerOfTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManagerOfTasks
+//
+//  Created by Антон Кирилюк on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
