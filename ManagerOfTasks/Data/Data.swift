a68a3e106344f35684d1730047fb67b9f6b0fdd4
kantosev <103773083+kantosev@users.noreply.github.com>
2022-04-18 20:50:20 +0300

    Созданы и заполнены ячейки

---
+++ ManagerOfTasks/Data/Data.swift
@@ -0,0 +1,12 @@
+//
+//  Data.swift
+//  ManagerOfTasks
+//
+//  Created by Антон Кирилюк on 18.04.2022.
+//
+
+import Foundation
+
+struct Tasks {
+    static let tasks: [String] = ["Выгулять собаку", "Покормить кота", "Сходить в магазин", "Убраться"]
+}
