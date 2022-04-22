a9ca4d613df2481beb494c312deafe0aed02fdf1
kantosev <103773083+kantosev@users.noreply.github.com>
2022-04-22 21:17:17 +0300

    Реализовано удаление задачи

--- ManagerOfTasks/Controllers/TableViewController.swift
+++ ManagerOfTasks/Controllers/TableViewController.swift
@@ -52,7 +52,15 @@ class TableViewController: UITableViewController {
         
     }
     
-
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "Удалить") { _,_,_ in
+            self.tasks.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        let actions = UISwipeActionsConfiguration(actions: [action])
+        return actions
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
